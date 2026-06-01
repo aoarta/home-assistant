537a28a044b6c32290775ba053ecceed8c941c4f
aoarta <naglzwick@gmail.com>
2026-06-01 17:15:06 +0200

    Rename; better forecast day after

--- find_unused_entities.py
+++ find_unused_entities.py
@@ -14,7 +14,7 @@ import json
 import re
 from pathlib import Path
 
-CONFIG_DIR = Path("/config")
+CONFIG_DIR = Path("./")
 STORAGE_DIR = CONFIG_DIR / ".storage"
 
 # Domains you usually do NOT want flagged even if unreferenced
